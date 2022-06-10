1acc1c23d9499cded84180df1910a5ad6f346942
Denis Ivanov <teniryte@gmail.com>
2022-06-10 12:57:31 +0300

    index

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const base64_1 = __importDefault(require("./base64"));
 const plural_1 = __importDefault(require("./plural"));
+const flatten_1 = __importDefault(require("./flatten"));
+const proto_1 = __importDefault(require("./proto"));
+const query_1 = __importDefault(require("./query"));
 exports.default = {
     base64: base64_1.default,
     plural: plural_1.default,
+    flatten: flatten_1.default,
+    proto: proto_1.default,
+    query: query_1.default,
 };
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import base64 from './base64';
 import plural from './plural';
+import flatten from './flatten';
+import proto from './proto';
+import query from './query';
 
 export default {
   base64,
   plural,
+  flatten,
+  proto,
+  query,
 };
